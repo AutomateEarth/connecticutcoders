04d8e50daf10b86819c9ab2e88ad348c2437e7fb
Jim <omesadev@outlook.com>
2021-12-20 17:35:41 -0500

    Added span styling

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import styled from 'styled-components';
 
 const BrandName = styled.h1`
     color: red;
+    font-weight: 200;
+`;
+
+const BrandSpan = styled.span`
+    font-weight: 800;
 `;
 
 const App = () => {
     return (
         <div>
             <header>
-                <BrandName>ConnecticutCoders</BrandName>
+                <BrandName>Connecticut<BrandSpan>Coders</BrandSpan></BrandName>
             </header>
             <main>
                 <p>Add card-like elements that bring users to a separate personal page with more information about a specific member. From there, users have an option to go to the portfolio, contact directly, or visit social media.</p>
