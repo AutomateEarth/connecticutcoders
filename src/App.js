7cfb5436394139899ba5429c987e4a3d54559d2b
Jim <omesadev@outlook.com>
2021-12-20 17:13:46 -0500

    Added Some Structure to the App.js File

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const BrandName = styled.h1`
+    color: red;
+`;
+
 const App = () => {
     return (
         <div>
-            <h1>Connecticut Coders</h1>
-            <p>Add some informational content here</p>
+            <header>
+                <BrandName>ConnecticutCoders</BrandName>
+            </header>
+            <main>
+                <p>Add card-like elements that bring users to a separate personal page with more information about a specific member. From there, users have an option to go to the portfolio, contact directly, or visit social media.</p>
+            </main>
+            <footer>
+                <p>Contact Information</p>
+                <p>Group email</p>
+                <p>Group phone number</p>
+            </footer>
         </div>
     );
 }
