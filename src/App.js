175e3b5471a712a55451ba153746b3b273376cc4
Jim <omesadev@outlook.com>
2022-01-11 15:38:57 -0500

    Ursula and Jim work#1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import React from 'react';
-
 import styled from 'styled-components';
+import headshotTest from './images/headshot-test.jpg';
+
+const Header = styled.h1`
+    padding: 15px;
+`;
 
 const BrandName = styled.h1`
-    color: red;
     font-weight: 200;
+    font-size: 2rem;
+    letter-spacing: 0.1rem;
+
 `;
 
 const BrandSpan = styled.span`
     font-weight: 800;
 `;
 
+const Container = styled.div`
+    margin: 20px;
+    padding: 20px;
+    border: 1px solid black;
+    height: 50vh;
+`;
+
+const ImgContainer = styled.div`
+    border: 1px solid black;
+    width: 200px;
+    height: 200px;
+`;
+
+const HeadshotImage = styled.img`
+    width: 100%;
+    height: 100%;
+`;
+
 const App = () => {
     return (
         <div>
-            <header>
+            <Header>
                 <BrandName>Connecticut<BrandSpan>Coders</BrandSpan></BrandName>
-            </header>
+            </Header>
             <main>
+                <div>
+                    <h3>Connecticut Codes was created in 2021.. blah blah blah</h3>
+                </div>
+                <Container>
+                    <ImgContainer>
+                        <HeadshotImage src={headshotTest} alt="Connecticut Codes member" />
+                    </ImgContainer>
+                    <h1>Name</h1>
+                    <h2>Bio - Add some information about yourself</h2>
+                    <p>Software development related media links</p>
+                    <p>Embedded Video</p>
+                </Container>
                 <p>Add card-like elements that bring users to a separate personal page with more information about a specific member. From there, users have an option to go to the portfolio, contact directly, or visit social media.</p>
             </main>
             <footer>
