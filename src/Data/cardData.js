b61c8856742a6fb3ac5f6b3ffec112b323d0d30f
Jim <omesadev@outlook.com>
2022-01-15 17:38:03 -0500

    Added hasPortfolio Boolean

--- src/Data/cardData.js
+++ src/Data/cardData.js
@@ -7,6 +7,7 @@ const cardInfo = [
         name: "Ursala", 
         image: "https://secure.meetupstatic.com/photos/member/4/8/2/3/member_305418467.jpeg", 
         bio: "She\'s awesome....Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor leo a diam sollicitudin tempor id eu nisl. Tempor nec feugiat nisl pretium fusce.",
+        hasPortfolio: true,
         github: "https://github.com/danyll39", 
         stackoverflow: "https://www.stackoverflow.com",
         learnMore: "http://ursalalindsey.com"
@@ -15,6 +16,7 @@ const cardInfo = [
         name: "Jim", 
         image: "https://secure.meetupstatic.com/photos/member/3/9/f/3/member_302354835.jpeg", 
         bio: "He\"s awesome too",
+        hasPortfolio: false,
         github: "https://github.com/danyll39", 
         stackoverflow: "https://www.stackoverflow.com",
         learnMore: "http://ursalalindsey.com"
@@ -23,6 +25,7 @@ const cardInfo = [
         name: "Justin L.", 
         image: "https://secure.meetupstatic.com/photos/member/a/f/3/6/member_306284854.jpeg", 
         bio: "Justin I'm great!",
+        hasPortfolio: true,
         github: "https://github.com/danyll39", 
         stackoverflow: "https://www.stackoverflow.com",
         learnMore: "http://ursalalindsey.com"
@@ -31,6 +34,7 @@ const cardInfo = [
         name: "Bryon", 
         image: "https://secure.meetupstatic.com/photos/member/e/4/4/c/member_308338444.jpeg", 
         bio: "Hello I'm Bryon", 
+        hasPortfolio: false,
         github: "https://github.com/danyll39", 
         stackoverflow: "https://www.stackoverflow.com",
         learnMore: "http://ursalalindsey.com"
@@ -39,6 +43,7 @@ const cardInfo = [
         name: "Justin D.", 
         image: "https://edit.co.uk/uploads/2016/12/Image-1-Alternatives-to-stock-photography-Thinkstock.jpg", 
         bio: "Hi, my name is also Justin", 
+        hasPortfolio: true,
         github: "https://github.com/justin107d", 
         stackoverflow: "https://www.stackoverflow.com",
         learnMore: "http://ursalalindsey.com"
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,8 @@
-import Profile from '../reuseableComponents/Profile';
+import { Card, Container, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { BsGithub, BsStackOverflow } from 'react-icons/bs';
+import cardInfo from '../Data/cardData';
 
-const HomePage = () => {
-
+export default function HomePage() {
     return (
         
         <div>
@@ -10,8 +11,34 @@ const HomePage = () => {
                 Come join us at the Starbucks in Rocky Hill on Sunday Mornings!
             </h3>
 
-            <main>   
-                <Profile/>
+            <main>
+                {cardInfo.map((card, i) => {
+                    return <Container>
+                        <Card style={{ width: '18rem' }} key={i}>
+                            <div className="embed-responsive embed-responsive-16by9">
+                                <Card.Img variant="top" src={card.image} />
+                                <Card.Body>
+                                    <Card.Title>{card.name}</Card.Title>
+                                    <Card.Text>
+                                        {card.bio}
+                                    </Card.Text>
+                                </Card.Body>
+                                {/* <ListGroup className="list-group-flush">
+                                <ListGroupItem>Cras justo odio</ListGroupItem>
+                                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                            </ListGroup> */}
+                                <Card.Body>
+                                    {card.hasPortfolio ? <Card.Link href={card.learnMore}>Portfolio</Card.Link> : null}         
+                                    <Card.Link href={card.github}><BsGithub /></Card.Link>
+                                    <Card.Link href={card.stackoverflow}><BsStackOverflow /></Card.Link>
+                                </Card.Body>
+                            </div>
+                        </Card>
+
+                    </Container>
+                })}
+                
             </main>
             
             <footer align="center">
@@ -22,15 +49,5 @@ const HomePage = () => {
             </footer>
 
         </div>
-
-    )
-};
-
-export default HomePage;
-
-/*
-
-
-            
-
-*/
+    );
+}
